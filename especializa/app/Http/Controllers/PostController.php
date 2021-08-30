59705b14f9c540f52e98e9885277092d592d27d5
Lucas Chalub <chalubaol9@gmail.com>
2021-08-30 18:42:17 -0300

    update project

--- especializa/app/Http/Controllers/PostController.php
+++ especializa/app/Http/Controllers/PostController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::get();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+
+        return redirect()->route('posts.index');
+    }
 }
